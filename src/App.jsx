ef3b9759661b98367d53e9292371e8354e316657
Rafael <127614067+rafaqfv@users.noreply.github.com>
2024-02-01 16:24:38 -0300

    Add api

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,32 @@ function App() {
   const [advice, setAdvice] = useState("Click on the Button");
 
   function api() {
-    const url = ""
+    setAdvice("Loading...");
+    const url = "https://api.adviceslip.com/advice";
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        const apiAdvice = data?.slip?.advice ?? "Nada";
+        setAdvice(apiAdvice);
+      })
+      .catch((error) => {
+        console.error("Erro ao fazer a solicitação:", error);
+      });
   }
 
   return (
     <>
-      <div className="w-full h-screen bg-slate-900 flex flex-col justify-center">
+      <div className="w-full h-screen bg-slate-900 flex flex-col justify-center items-center">
         <h1 className="text-4xl text-white font-semibold text-center">
           Advices
         </h1>
         <p className="text-white font-light text-center">{advice}</p>
+        <button
+          className="absolute bottom-4 bg-yellow-500 rounded-lg px-4 py-2"
+          onClick={api}
+        >
+          Get an advice
+        </button>
       </div>
     </>
   );
