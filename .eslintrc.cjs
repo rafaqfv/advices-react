7699fddc31074bc965ccb7fa4968b4fa04a9c342
Rafael <127614067+rafaqfv@users.noreply.github.com>
2024-02-07 21:37:51 -0300

    Add notes README.md

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,15 +6,22 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'prettier/prettier': ["error", {
+      'printWidth': 80,
+      'tabWidth': 2,
+      'singleQuote': false,
+      'trailingComma': 'all',
+      'arrowParens': 'always',
+      'semi': true,
+      'endOfLine': 'auto',
+    }],
   },
 }
+
--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # React + Advices
 
 This is my first react project, and with an advice api
+
+What i learned here?
+Deploy with gh-pages with `npm run deploy`
+```
+  "homepage": "https://${USUÁRIO}.github.io/${caminho do repositório}",
+  "name": "project-name",
+  "private": true,
+  "version": "0.0.0",
+  "type": "module",
+  "scripts": {
+    "dev": "vite",
+    "build": "vite build",
+    "lint": "eslint . --ext js,jsx --report-unused-disable-directives --max-warnings 0",
+    "preview": "vite preview",
+    "predeploy": "npm run build",
+    "deploy": "gh-pages -d dist"
+  },
+```
